f34d4141bdd660c44d721956a9e864bf8870b231
claudiaaziz <claudiahannahh@gmail.com>
2023-09-22 14:49:16 -0400

    Add methods to 01 queries

--- movie_buff/01_queries.rb
+++ movie_buff/01_queries.rb
@@ -7,6 +7,8 @@ def it_was_ok
   #
   # Find the id, title, and score of all movies with scores between 2 and 3.
 
+  Movie.select(:id, :title, :score)
+  .where(score: 2..3)
 end
 
 def harrison_ford
@@ -21,6 +23,10 @@ def harrison_ford
   # Find the id and title of all movies in which Harrison Ford appeared but not
   # as a lead actor.
 
+  Actor
+  .joins(:movies)
+  .select("movies.id, movies.title") 
+  .where("actors.name = 'Harrison Ford' AND castings.ord != 1")
 end
 
 def biggest_cast
@@ -37,7 +43,8 @@ def biggest_cast
   #
   # Find the id and title of the 3 movies with the largest casts (i.e., most
   # actors).
-  
+    Actor
+    .select("movies.id, movies.title").joins(:movies).group("movies.id").order("COUNT(movies.id) DESC").limit(3)
 end
 
 def directed_by_one_of(them)
@@ -54,7 +61,8 @@ def directed_by_one_of(them)
   # Find the id and title of all the movies directed by one of 'them'.
   
   # Note: Directors appear in the 'actors' table.
-
+  Movie
+  .select(:id, :title).joins(:director).where("actors.name IN (?)", them)
 end
 
 def movie_names_before_1940
